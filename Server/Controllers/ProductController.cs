a87bc91e743fec6c2a1598fc5a20ac9343178729
LucaQueiroz <luca-queiroz@univem.edu.br>
2020-10-06 22:53:09 -0300

    first commit

--- Server/Controllers/ProductController.cs
+++ Server/Controllers/ProductController.cs
@@ -4,27 +4,38 @@ using MySQLcrudBlazor.Shared;
 using System;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 [ApiController]
 [Route("[controller]")]
 public class ProductController : Controller
 {
     private readonly AppDbContext db;
-    
+
     public ProductController(AppDbContext db)
     {
         this.db = db;
     }
 
-[HttpGet]
-[Route("List")]
-public async Task<IActionResult> Get()
+    [HttpGet]
+    [Route("List")]
+    public async Task<IActionResult> Get()
     {
         var products = await db.Products.ToListAsync();
         return Ok(products);
     }
 
-public async Task<ActionResult> Post([FromBody] Product product)
+    [HttpGet]
+    [Route("GetById")]
+    public async Task<IActionResult> Get([FromQuery] string id)
+    {
+        var product = await db.Products.SingleOrDefaultAsync(x => x.Id == Convert.ToInt32(id));
+        return Ok(product);
+    }
+
+    [HttpPost]
+    [Route("Create")]
+    public async Task<ActionResult> Post([FromBody] Product product )
     {
         try
         {
@@ -36,14 +47,55 @@ public async Task<ActionResult> Post([FromBody] Product product)
             };
 
             db.Add(newProduct);
-            await db.SaveChangesAsync();
+            await db.SaveChangesAsync();//INSERT INTO
             return Ok();
         }
-        catch(Exception e)
+        catch (Exception e)
         {
             return View(e);
         }
-
-        
     }
-}
+
+    [HttpPut]
+    [Route("Update")]
+    public async Task<IActionResult> Put([FromBody] Product product)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        db.Entry(product).State = EntityState.Modified;
+        try
+        {
+            await db.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            throw (ex);
+        }
+        return NoContent();
+    }
+
+    [HttpDelete]
+    [Route("Delete/{id}")]
+    public async Task<ActionResult<Product>> Delete(int id)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        var product = await db.Products.FindAsync(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+        db.Products.Remove(product);
+        await db.SaveChangesAsync();
+        return product;
+    }
+    private bool UserExists(int id)
+    {
+        return db.Users.Any(e => e.Id == id);
+    }
+}    
--- Shared/Product.cs
+++ Shared/Product.cs
@@ -9,7 +9,7 @@ namespace MySQLcrudBlazor.Shared
     public class Product
     {
 
-        public int id { get; set; }
+        public int Id { get; set; }
 
         [Required]
         public string Name { get; set; }
